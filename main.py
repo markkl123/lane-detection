43248ff1a8a072ece5e8e58dfdc9ada836fc15e1
Mark Klaiman <markkl1998@gmail.com>
2022-12-01 13:38:06 +0200

    model on single photo

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+
+figsize = (10, 10)
+def print_img(image,name="image"):
+    plt.figure(figsize=figsize)
+    plt.imshow(image)
+    plt.title(name)
+    plt.show()
 def load_video_frames(filename):
-    pass
+    im = cv2.imread(filename)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+
+    print_img(im,"original image")
+    return im
 
 
 def preprocess_frames(raw_frames):
-    res_frames = list()
+    im_size = raw_frames.shape[:2]
+    cropped_image = raw_frames[(62 * im_size[0]) // 100:, (0*im_size[1] // 100): (100*im_size[1] //100), :]
+    print_img(cropped_image, "cropped_image")
+    filtered_image = cv2.inRange(cropped_image, np.array([200,200,200]),np.array([255,255,255]))
+    print_img(filtered_image, "filtered_image")
+    kernel = np.ones((5,1))
+    eroded_im = cv2.dilate(filtered_image,kernel)
+    print_img(eroded_im, "eroded_im")
+    res_frames = filtered_image
     # Ideas:
         # Crop image
         # Filter only certain color pixels
@@ -13,19 +35,70 @@ def preprocess_frames(raw_frames):
             # Intensify dark color?
             # Contrast?
 
-    return res_frames
+    return res_frames,cropped_image
 
+def line_filter(lines):
+    res = []
+    for line in lines[:, 0, :]:
 
-def detect_lanes(raw_frames):
+        if (0.85<line[1]<1.08 or 2.05<line[1]<2.25) and \
+                (len(res) == 0 or
+                np.all(np.abs(line[0] - np.array(res)[:, 0])> 250)):
+            res.append(line)
+    return np.array(res[:2])
+
+def Draw_area(img,lines):
+    h, w = img.shape[:2]
+    r1,t1 = lines[0]
+    r2, t2 = lines[1]
+
+    x = np.arange(w)
+    line1 = lambda x: (r1-x*np.cos(t1))/np.sin(t1)
+    line2 = lambda x: (r2-x*np.cos(t2))/np.sin(t2)
+    y1 = line1(x)
+    y2 = line2(x)
+
+    points1 = np.array([[[xi, yi]] for xi, yi in zip(x, y1) if (0 <= xi < w and 0 <= yi < h)]).astype(np.int32)
+    points2 = np.array([[[xi, yi]] for xi, yi in zip(x, y2) if (0 <= xi < w and 0 <= yi < h)]).astype(np.int32)
+    points = np.concatenate((points1, points2))
+
+    drawn_image = img.copy()
+    cv2.fillPoly(drawn_image, [points], color=[255, 0, 255])
+    print_img(drawn_image,"drawn_image")
+    return drawn_image
+def detect_lanes(raw_frames,cropped_im,origin_im):
+    r_step, t_step, TH = 1, np.pi/180, 40
+    lines = cv2.HoughLines(raw_frames, r_step, t_step, TH)
+    lines = line_filter(lines)
+    # lines = lines[:, 0, :]
+    print(lines)
+    for r_t in lines:
+        rho = r_t[0]
+        theta = r_t[1]
+
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 2000 * (-b))
+        y1 = int(y0 + 2000 * (a))
+        x2 = int(x0 - 2000 * (-b))
+        y2 = int(y0 - 2000 * (a))
+
+        res = cv2.line(cropped_im, (x1, y1), (x2, y2), (0, 0, 255), thickness=5)
+    print_img(res, "res")
+    drawn_image = Draw_area(cropped_im,lines)
+    im_size = origin_im.shape[:2]
+    origin_im[(62 * im_size[0]) // 100:, (0 * im_size[1] // 100): (100 * im_size[1] // 100), :] = drawn_image
+    print_img(origin_im, "final result")
     res_frames = list()
     # How to find best lines?
-        # Canny
         # Hough Parabula ?? self implement (Omri) or use HoughLines (Mark)
     # How to detect lane transition?
         # transform lines from (x,y) to (rho, theta) and detect changes in rho (distance to origin)
             # Distance is decreasing -> LEFT
             # Distance is increasing -> RIGHT 
-            # Use threshold to deal with noise
+            # Use threshold to deal with noise USED!!!
     # How to follow correct lanes during transition?
         # Compare to last frame
         # Lanes change will happen naturally when transition is complete
@@ -37,10 +110,12 @@ def save_frames_to_video(frames):
 
 
 if __name__ == '__main__':
-    video_filename = "video.mp3"
-    raw_frames = load_video_frames(video_filename)
+    video_filename = "Test_Photo4.png" #"video.mp3"
 
-    res_frames = preprocess_frames(raw_frames)
-    final_frames = detect_lanes(res_frames)
+    res_frames = load_video_frames(video_filename)
+
+    preprocessed_frames, cropped_image = preprocess_frames(res_frames)
+
+    final_frames = detect_lanes(preprocessed_frames, cropped_image,res_frames)
 
     save_frames_to_video(final_frames)
